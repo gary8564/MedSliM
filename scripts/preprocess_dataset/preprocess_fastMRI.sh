b8747bbb305f44ccac0e91d0d5504cf2107db6bc
Gary Chang <54540162+gary8564@users.noreply.github.com>
2026-04-13 02:43:36 +0200

    Delete scripts/preprocess_dataset/preprocess_fastMRI.sh

--- scripts/preprocess_dataset/preprocess_fastMRI.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=64G
-#SBATCH --time=24:00:00
-#SBATCH --job-name=preprocess_fastMRI_train
-#SBATCH --output=stdout_preprocess_fastMRI_train_%j.txt
-
-source .venv/bin/activate
-python scripts/preprocess_dataset/preprocess_fastMRI.py --split train --workers 32
